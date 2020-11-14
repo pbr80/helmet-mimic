c76ceb11dd173fd2f47f6aabcd2ab782964ad69c
pbr80 <obradyp@gmail.com>
2020-11-14 10:14:28 -0500

    added bio section

--- helmet-mimic.py
+++ helmet-mimic.py
@@ -5,6 +5,7 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+import textwrap
 
 address = "https://2e.aonprd.com/Monsters.aspx?ID="
 n = str(random.randint(1, 1003))
@@ -13,11 +14,16 @@ page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 main = soup.find(id='main')
-bio = main.find(id='ctl00_MainContent_DetailedOutput')
-title = bio.find('h1', class_='title').get_text()
-get_spans = bio.find_all('span')
+content = main.find(id='ctl00_MainContent_DetailedOutput')
+title = content.find('h1', class_='title').get_text()
+bio = content.find('h1', class_='title').next_sibling
+
+get_spans = content.find_all('span')
 statblock = [span.get_text() for span in get_spans]
-print(url)
+
 print(title)
+print(url)
+print('\n' + textwrap.fill(bio) + '\n')
+
 for stat in statblock:
-    print(stat)
+    print(textwrap.fill(stat))
